b2b44e16057ec3cceb1d6b44a387b17a6cb4c656
Alintong <62975670+alintong-0@users.noreply.github.com>
2022-12-12 10:04:19 +0800

    Create zipRelease.py

---
+++ zipRelease.py
@@ -0,0 +1,9 @@
+import shutil
+ 
+# 函数
+shutil.make_archive('./release.zip', 'zip', './out/Release/')
+ 
+# 注释
+# zipfile_path = 'E:\YQ_HLW'        打包后保存的文件路径及名称
+# 'zip' ： 打包后的文件格式，也可为tar等
+# Date_file ='E:\DataFile1'   被打包的文件夹名称
