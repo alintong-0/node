9c86568a580283b668d8e571ddec02ac0aaac7e4
Alintong <62975670+alintong-0@users.noreply.github.com>
2022-12-12 11:21:51 +0800

    Update zipRelease.py

--- zipRelease.py
+++ zipRelease.py
@@ -1,7 +1,7 @@
 import shutil
  
 # 函数
-shutil.make_archive('./release.zip', 'zip', './out/Release/')
+shutil.make_archive('./release', 'zip', './out/Release/')
  
 # 注释
 # zipfile_path = 'E:\YQ_HLW'        打包后保存的文件路径及名称
